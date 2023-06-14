a7cae5bd5966a88043eee442a40ec410e1383e40
alif-gunadi <alifgunadi1303@gmail.com>
2023-06-14 14:36:04 +0700

    5th variable

---
+++ 5/variable.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Muhammad Alif Gunadi"
+	fmt.Println(name)
+
+	name = "Jage Weist"
+	fmt.Println(name)
+
+	var siblingName = "Zaqi Gunadi"
+	fmt.Println(siblingName)
+
+	var age = 25
+	fmt.Println("My Age is", age)
+
+	country := "I'm from Indonesia"
+	fmt.Println(country)
+
+	var (
+		firstname = "Difa"
+		lastname = "Gunadi"
+	)
+	fmt.Println(firstname)
+	fmt.Println(lastname)
+}
